c0cb34945334b46950fd3a58a6d100188f2a7bd3
fboyle2001 <finlayboyle2001@gmail.com>
2020-02-06 17:18:47 +0000

    Task 3 done

--- nj_task.py
+++ nj_task.py
@@ -17,13 +17,44 @@ class Matrix:
                 self.data.append(row)
 
     def __str__(self):
-        out = ""
-        
+        max_char_size = 0
+
         for row in self.data:
+            mi = len(str(min(row)))
+            ma = len(str(max(row)))
+
+            if mi > max_char_size:
+                max_char_size = mi
+
+            if ma > max_char_size:
+                max_char_size = ma
+
+        display_rows = []
+        
+
+        for row in self.data:
+            disp_row = []
+            for char in row:
+                c = str(char)
+
+                c = (max_char_size - len(c) + 1) * " " + c
+                disp_row.append(c)
+            display_rows.append(disp_row)
+
+        out = ""
+
+        for row in display_rows:
             if out != "":
                 out += "\n"
+
+            out += "["
                 
-            out += str([x if x != None else " " for x in row ])
+            for c in row:
+                #if out[-1] != "[":
+                #    out += ","
+                out += c
+
+            out += "]"
 
         return out
 
@@ -73,11 +104,14 @@ class Matrix:
         return (max_row, c_index)
 
     def find_min_pos(self):
+        """
+        Finds min position excluding the diagonal elements
+        """
         min_row = 0
         min_value = None
 
         for i, row in enumerate(self.data):
-            m = min(row)
+            m = min(row[:i] + row[i+1:])
             if min_value == None or m < min_value:
                 min_value = m
                 min_row = i
@@ -133,6 +167,13 @@ class Matrix:
             
     def add_row(self, data=[]):
         self.data.append(data)
+
+    def replace_row(self, row, data):
+        self.data[row] = data
+
+    def replace_col(self, col, data):
+        for i in range(0, self.col_length()):
+            self.data[i][col] = data[i]
         
 def NJ():
     relations = Matrix(8, 8 , [
@@ -147,62 +188,74 @@ def NJ():
         ])
 
     q_scores = Matrix(relations.row_length(), relations.col_length())
-    r = relations.row_length()
 
-    biggest_q = 0
+    while q_scores.row_length() != 2:
+        r = relations.row_length()
 
-    for i in range(0, q_scores.col_length()):
-        for j in range(i + 1, q_scores.row_length()):
-            q = (r - 1) * relations.get(i, j) - relations.row_sum(i) - relations.col_sum(j)
+        for i in range(0, q_scores.col_length()):
+            q_scores.update(i, i, 0)
+            for j in range(i + 1, q_scores.row_length()):
+                q = (r - 1) * relations.get(i, j) - relations.row_sum(i) - relations.col_sum(j)
+                    
+                q_scores.update(i, j, q)
+                q_scores.update(j, i, q)
+        
+        print("n =", relations.col_length())
+        print("Relation Matrix:")
+        print(relations)
+        print()
+        print("Q-Score Matrix:")
+        print(q_scores)
+        print()
+
+        #have relation scores and q_scores
+
+        min_x, min_y = q_scores.find_min_pos()
+
+        #going to remove these 2 so need to work out the scores first
+
+        new_scores = []
+
+        #calculate the d(ab, c) scores
+
+        for i in range(0, q_scores.col_length()):
+            if i == min_x or i == min_y:
+                continue
             
-            if q > biggest_q:
-                biggest_q = q
-                
-            q_scores.update(i, j, q)
-            q_scores.update(j, i, q)
+            dab_c = relations.get(min_x, i) + relations.get(min_y, i) - relations.get(min_x, min_y)
+            dab_c /= 2
+            new_scores.append(dab_c)
 
-    for i in range(0, q_scores.col_length()):
-        q_scores.update(i, i, biggest_q + 1)
+        #print("Matrix is", relations.row_length(), relations.col_length())
 
-    min_x, min_y = q_scores.find_min_pos()
+        #delete the species with a higher index in the matrix
+        #avoids having to correc min_y or min_x since they cannot equal each other
 
-    print(min_x, min_y)
-    print(relations)
-
-    #could min_y = min_x? don't think so
-
-    if min_y > min_x:
-        relations.delete_row(min_y)
-        relations.delete_col(min_y)
-        relations.delete_row(min_x)
-        relations.delete_col(min_x)
-    else:
-        relations.delete_row(min_x)
-        relations.delete_col(min_x)
-        relations.delete_row(min_y)
-        relations.delete_col(min_y)
-
-    #now compute the new d(ab,c)
-
-    #now need to shrink the relations matrix
-    #then recompute the q scores matrix
-    #repeat until 2x2
-    #no efficiency marks for this
-
-    print(relations)
-    print(relations.row_length(), relations.col_length())
-
-    relations.add_row([0, 0, 0, 0, 0, 0, 0])
-    relations.add_col([0, 0, 0, 0, 0, 0, 0])
-
-    print(relations)
-    print(relations.row_length(), relations.col_length())
-
-    row = relations.row_length() - 1
-
-    #last spot is going to be 0
-    for i in range(row):
-        pass
+        if min_y > min_x:
+            relations.delete_row(min_y)
+            relations.delete_col(min_y)
+            
+            q_scores.delete_row(min_y)
+            q_scores.delete_col(min_y)
+            
+            new_scores.insert(min_x, 0)
+            
+            relations.replace_row(min_x, new_scores)
+            relations.replace_col(min_x, new_scores)
+        else:
+            relations.delete_row(min_x)
+            relations.delete_col(min_x)
+            
+            q_scores.delete_row(min_x)
+            q_scores.delete_col(min_x)
+            
+            new_scores.insert(min_y, 0)
+            
+            relations.replace_row(min_y, new_scores)
+            relations.replace_col(min_y, new_scores)
+            
+        #print("Matrix is", relations.row_length(), relations.col_length())
+        #print(relations)
 
     return relations
 
